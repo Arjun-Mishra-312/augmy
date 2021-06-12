c059aed0b5b6713b19730682d543633c94e8c28b
Arjun-Mishra-312 <arjunmishra31204@gmail.com>
2021-06-13 00:00:16 +0530

    3D model widget update

--- lib/echo_ar.dart
+++ lib/echo_ar.dart
@@ -4,7 +4,7 @@ import 'package:flutter_3d_obj/flutter_3d_obj.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 
 class EchoARmodels extends StatefulWidget {
-  EchoARmodels({Key key, this.keyword,this.keywords}) : super(key: key);
+  EchoARmodels({Key key, this.keyword, this.keywords}) : super(key: key);
 
   final String keyword;
   final List<String> keywords;
@@ -25,12 +25,11 @@ class _EchoARmodelsState extends State<EchoARmodels> {
   void _incrementCounter() async {
     if (widget.echoAR == null)
       widget.echoAR = EchoAR(apiKey: DotEnv.env['APIKEY']);
-    String path = await widget.echoAR
-        .getModelFromEntryId(DotEnv.env['Model1']);
-    String path2 = await widget.echoAR
-        .getModelFromEntryId(DotEnv.env['Model2']);
-    String path3 = await widget.echoAR
-        .getModelFromEntryId(DotEnv.env['Model3']);
+    String path = await widget.echoAR.getModelFromEntryId(DotEnv.env['Model1']);
+    String path2 =
+        await widget.echoAR.getModelFromEntryId(DotEnv.env['Model2']);
+    String path3 =
+        await widget.echoAR.getModelFromEntryId(DotEnv.env['Model3']);
 
     setState(() {
       _modelHologramPath = path;
@@ -43,108 +42,118 @@ class _EchoARmodelsState extends State<EchoARmodels> {
   }
 
   @override
+  void initState() {
+    setState(() {
+      isLoading = true;
+    });
+    // TODO: implement initState
+    super.initState();
+    _incrementCounter();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: isLoading
-            ? Center(child: CircularProgressIndicator())
-            : Center(
-                child: (_modelHologramPath == "" &&
-                        _modelHologramPath2 == "" &&
-                        _modelHologramPath3 == "")
-                    ? Text("Press the button to start")
-                    : ListView(
-                        padding: EdgeInsets.all(15.0),
-                        children: <Widget>[
-                          SizedBox(
-                            height: 100.0,
+    return isLoading
+        ? Center(child: CircularProgressIndicator())
+        : GridView.count(
+            childAspectRatio: 0.753,
+            crossAxisCount: 2,
+            crossAxisSpacing: 60.0,
+            mainAxisSpacing: 30.0,
+            padding: EdgeInsets.all(30.0),
+            children: <Widget>[
+              _modelHologramPath3 != ""
+                  ? Container(
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(255, 255, 255, 0.1),
+                        borderRadius: BorderRadius.circular(8.0),
+                        border: Border.all(
+                            color: Color.fromRGBO(255, 255, 255, 0.3),
+                            width: 1.64),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Object3D(
+                            size: const Size(100.0, 100.0),
+                            path: _modelHologramPath3,
+                            asset: true,
+                            zoom: 100.0,
                           ),
-                          _modelHologramPath3 != ""
-                              ? Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.yellow,
-                                    borderRadius: BorderRadius.circular(25),
-                                  ),
-                                  child: Row(
-                                    children: [
-                                      Object3D(
-                                        size: const Size(100.0, 100.0),
-                                        path: _modelHologramPath3,
-                                        asset: true,
-                                        zoom: 80.0,
-                                      ),
-                                      Text('Primitive Cubic Unit Cell')
-                                    ],
-                                  ),
-                                )
-                              : SizedBox(
-                                  height: 1.0,
-                                ),
-                          SizedBox(
-                            height: 50.0,
-                          ),
-                          _modelHologramPath != ""
-                              ? Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.red,
-                                    borderRadius: BorderRadius.circular(25),
-                                  ),
-                                  child: Row(
-                                    children: [
-                                      Object3D(
-                                        size: const Size(100.0, 100.0),
-                                        path: _modelHologramPath,
-                                        asset: true,
-                                        zoom: 80.0,
-                                      ),
-                                      Text('Body Centered Cubic Unit Cell')
-                                    ],
-                                  ),
-                                )
-                              : SizedBox(
-                                  height: 1.0,
-                                ),
-                          SizedBox(
-                            height: 50.0,
-                          ),
-                          _modelHologramPath2 != ""
-                              ? Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.blue,
-                                    borderRadius: BorderRadius.circular(25),
-                                  ),
-                                  child: Row(
-                                    children: [
-                                      Object3D(
-                                        size: const Size(100.0, 100.0),
-                                        path: _modelHologramPath2,
-                                        asset: true,
-                                        zoom: 80.0,
-                                      ),
-                                      Text('Face Centered Cubica Unit Cell')
-                                    ],
-                                  ),
-                                )
-                              : SizedBox(
-                                  height: 1.0,
-                                ),
-                          SizedBox(
-                            height: 50.0,
-                          ),
+                          Text(
+                            'Primitive Cubic Unit Cell',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.white),
+                          )
                         ],
                       ),
+                    )
+                  : SizedBox(
+                      height: 1.0,
+                    ),
+              _modelHologramPath != ""
+                  ? Container(
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(255, 255, 255, 0.1),
+                        borderRadius: BorderRadius.circular(8.0),
+                        border: Border.all(
+                            color: Color.fromRGBO(255, 255, 255, 0.3),
+                            width: 1.64),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Object3D(
+                            size: const Size(100.0, 100.0),
+                            path: _modelHologramPath,
+                            asset: true,
+                            zoom: 100.0,
+                          ),
+                          Text(
+                            'Body Centered Cubic Unit Cell',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      ),
+                    )
+                  : SizedBox(
+                      height: 1.0,
+                    ),
+              _modelHologramPath2 != ""
+                  ? Container(
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(255, 255, 255, 0.1),
+                        borderRadius: BorderRadius.circular(8.0),
+                        border: Border.all(
+                            color: Color.fromRGBO(255, 255, 255, 0.3),
+                            width: 1.64),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Object3D(
+                            size: const Size(100.0, 100.0),
+                            path: _modelHologramPath2,
+                            asset: true,
+                            zoom: 100.0,
+                          ),
+                          Text(
+                            'Face Centered Cubica Unit Cell',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      ),
+                    )
+                  : SizedBox(
+                      height: 1.0,
+                    ),
+              SizedBox(
+                height: 50.0,
               ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              isLoading = true;
-            });
-            _incrementCounter();
-          },
-          tooltip: 'Increment',
-          child: Icon(Icons.file_download),
-        ),
-      ),
-    );
+            ],
+          );
   }
 }
